c72270243b722a42b35137201842e3c19e168950
Kiran_project <kkabdal1@gmail.com>
2024-03-30 14:52:23 +0100

    quit option

--- MiniProject.py
+++ MiniProject.py
@@ -4,8 +4,10 @@ target = random.randint(1,100)
 #print(target)
 
 while True:
-    user = int(input("Enter a number : "))
-
+    user = (input("Enter a number or Q : "))
+    if user == "Q":
+        break
+    user = int(user)
     if (user == target):
         print("You Won the challange by guessing the correct number - chocolate gift", user)
         break
